24e6b08d5379ebe83a36922664869cc6861e3b0a
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2023-07-10 11:13:40 +0300

    add assets path

---
+++ lib/presentation/theme/assets_manager.dart
@@ -0,0 +1,20 @@
+class AssetsManager {
+  static const String phone = 'assets/img/phone.svg';
+  static const String forget = 'assets/img/forget.svg';
+  static const String createPassword = 'assets/img/create_password.svg';
+  static const String order = 'assets/img/order.svg';
+  static const String bagNavigation = 'assets/icon/navigation_bar/bag.svg';
+  static const String homeNavigation = 'assets/icon/navigation_bar/home.svg';
+  static const String menuNavigation = 'assets/icon/navigation_bar/menu.svg';
+  static const String inactiveNavigation =
+      'assets/icon/navigation_bar/inactive.svg';
+  static const String callProfile = 'assets/icon/profile/call.svg';
+  static const String editProfile = 'assets/icon/profile/edit.svg';
+  static const String logoutProfile = 'assets/icon/profile/logout.svg';
+  static const String powerOffProfile = 'assets/icon/profile/logout.svg';
+  static const String locationProfile = 'assets/icon/profile/location.svg';
+  static const String chatProfile = 'assets/icon/profile/chat.svg';
+  static const String mailProfile = 'assets/icon/profile/mail.svg';
+  static const String shoppingCartProfile =
+      'assets/icon/profile/shopping_cart.svg';
+}
