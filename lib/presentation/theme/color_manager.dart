2f538b5b208bb22a66c4f2231b8a0c1f4eb3ba6a
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2023-07-10 11:17:43 +0300

    style: add color used in the app

---
+++ lib/presentation/theme/color_manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  // pick color from RGB
+  static const Color btnPrimary = Color.fromRGBO(94, 196, 1, 1);
+  static const Color btnSecondary = Color.fromRGBO(243, 122, 32, 1);
+  static const Color removeBtn = Color.fromRGBO(255, 85, 82, 1);
+  static const Color callBtn = Color.fromRGBO(54, 179, 126, 1);
+  static const Color primary = Color.fromRGBO(196, 196, 196, 1);
+  static const Color productName = Color.fromRGBO(26, 38, 73, 255);
+}
